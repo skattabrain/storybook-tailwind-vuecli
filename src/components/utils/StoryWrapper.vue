84a493c53600b440a2b508573855d99ceda33e0f
Greg Guepy <greg@guepy.com>
2020-04-12 20:24:46 -0400

    story utils

---
+++ src/components/utils/StoryWrapper.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="min-h-screen w-screen py-3 bg-gray-300 flex flex-col items-center justify-center">
+    <div :class="wrapperWidthClasses">
+      <slot></slot>
+    </div>
+    <ScreenSizeIndicator />
+  </div>
+</template>
+
+<script>
+import ScreenSizeIndicator from '@/components/utils/ScreenSizeIndicator.vue';
+
+export default {
+  name: 'StoryWrapper',
+  components: {
+    ScreenSizeIndicator
+  },
+  props: {
+    wrapperWidthClasses: {
+      default: 'w-1/2',
+      type: String,
+    },
+  },
+};
+</script>
