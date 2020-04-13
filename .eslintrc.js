a8f91a3f8ef2290f20c09b8b5f419c5c61cbc63b
Greg Guepy <greg@guepy.com>
2020-04-12 20:25:11 -0400

    remove airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,25 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     'eslint:recommended',
   ],
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
     quotes: ['error', 'single', { allowTemplateLiterals: true }],
   },
+
   overrides: [
     {
       files: [
