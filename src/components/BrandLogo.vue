5718f9e961f1a64b2e425d36e6d445d582642b31
Greg Guepy <greg@guepy.com>
2020-04-13 20:34:35 -0400

    logo component

---
+++ src/components/BrandLogo.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class="brand-logo" :class="[dark ? 'text-white' : 'text-dark']">
+    WE<span class="text-purple-500">SELL</span>LIMOS<span class="text-purple-500">.COM</span>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'BrandLogo',
+  props: {
+    dark: {
+      default: true,
+      type: Boolean
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+  .brand-logo {
+    font-weight: 800;
+    font-family: "Open Sans", sans-serif;
+  }
+</style>
