59ad1cf0e1ab910552f7ce2f1493f1c87036d42d
Greg Guepy <greg@guepy.com>
2020-04-22 08:53:38 -0400

    testing upgrade to SB 6 alpha 41

--- src/components/LayoutApp.vue
+++ src/components/LayoutApp.vue
@@ -2,7 +2,11 @@
   <div class="flex flex-col min-h-screen bg-gray-200">
     <layout-navigation :items="navItems" />
     <layout-header :heading="heading" />
-    <layout-main class="flex-grow" />
+    <layout-main class="flex-grow">
+      <template v-slot:stuff>
+        <h1>Here might be a page title</h1>
+      </template>
+    </layout-main>
     <layout-footer />
   </div>
 </template>
--- src/components/LayoutMain.vue
+++ src/components/LayoutMain.vue
@@ -2,6 +2,7 @@
   <div class="">
     <div class="container">
       Main
+      <slot name="stuff">Stuff goes here</slot>
     </div>
   </div>
 </template>
