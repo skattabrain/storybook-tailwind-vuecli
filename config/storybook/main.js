099f083edac459d22fc4e2aebb9a95c226cf3281
Greg Guepy <greg@guepy.com>
2020-04-24 20:00:11 -0400

    Include some essential addons, clean up the preview

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,25 +1,13 @@
 module.exports = {
   stories: ['../../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
   addons: [
-    // '@storybook/addon-actions',
-    // {
-    //   name: '@storybook/addon-docs',
-    //   options: {
-    //     babelOptions: {
-    //       presets: [
-    //         [
-    //           '@vue/cli-plugin-babel/preset',
-    //           {
-    //             jsx: false,
-    //           },
-    //         ],
-    //       ],
-    //     },
-    //   },
-    // },
-    '@storybook/addon-docs',
     '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-storysource',
+    '@storybook/addon-viewport',
   ],
 };
--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,2 +1,14 @@
 import '../../src/styles.css';
-import './storybook.css';
+import { addParameters } from '@storybook/vue';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+addParameters({
+  backgrounds: [
+    { name: 'twitter', value: '#00aced', default: true },
+    { name: 'facebook', value: '#3b5998' },
+  ],
+  layout: 'centered',
+  viewport: {
+    viewports: INITIAL_VIEWPORTS
+  },
+});
