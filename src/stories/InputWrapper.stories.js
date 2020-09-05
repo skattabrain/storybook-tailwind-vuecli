b3f7f3dbec775a591edb09d3a41a625dbcef7350
Greg Guepy <greg@guepy.com>
2020-09-05 12:29:55 -0400

    story update

--- src/stories/InputWrapper.stories.js
+++ src/stories/InputWrapper.stories.js
@@ -2,7 +2,7 @@ import InputWrapper from '@/components/InputWrapper.vue';
 
 export default {
   component: InputWrapper,
-  title: 'Base/Input Wrapper',
+  title: 'Components/Input Wrapper',
   argTypes: {
     disabled: { control: 'boolean', name: 'Is disabled?' },
     showWithOtherControls: { control: 'boolean', name: 'Shot with other controls' },
@@ -13,7 +13,7 @@ export default {
   args: {
     disabled: false,
     showWithOtherControls: false,
-    help: 'Enter your name above or you will be sorry.',
+    help: 'Enter your last name here. If this is a problem, sorry but we can\'t help you.',
     label: 'Last Name',
     error: null,
   }
@@ -40,16 +40,16 @@ export const Text = (args) => ({
   },
   template: `
 <div class="p-6">
-  <input-wrapper v-if="showWithOtherControls" class="mb-12" :is-disabled="disabled" label="First Name" help="First name">
-    <input :disabled="disabled" class="w-full" type="text" placheolder="John">
+  <input-wrapper v-if="showWithOtherControls" class="mb-3" :is-disabled="disabled" label="First Name" help="If you do not know your first name, please seek professional help.">
+    <input :disabled="disabled" class="w-full" type="text" placeholder="John">
   </input-wrapper>
 
-  <input-wrapper :is-disabled="disabled" :error="error" :label="label" :help="help">
-    <input :disabled="disabled" class="w-full" type="text" placheolder="Doe">
+  <input-wrapper class="mb-3" :is-disabled="disabled" :error="error" :label="label" :help="help">
+    <input :disabled="disabled" class="w-full" type="text" placeholder="Doe">
   </input-wrapper>
 
-  <input-wrapper v-if="showWithOtherControls" class="mt-12" label="Email">
-    <input :disabled="disabled" class="w-full" type="text" placheolder="Some other value">
+  <input-wrapper v-if="showWithOtherControls" class="mb-3" label="Email" help="So we can SPAM the shit out of you.">
+    <input :disabled="disabled" class="w-full" type="text" placeholder="johndoe@example.com">
   </input-wrapper>
 </div>
 `
