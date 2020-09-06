686b1178434cea39f6d9a8cf59ea570ca5b753df
Greg Guepy <greg@guepy.com>
2020-09-05 21:04:41 -0400

    primping

--- src/components/utils/ScreenSizeIndicator.vue
+++ src/components/utils/ScreenSizeIndicator.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="absolute bottom-0 right-0 text-gray-300 m-1 p-1 rounded bg-gray-800 bg-opacity-50">
-    <span class="indicator mr-2 text-gray-100 sm:text-gray-300">
+  <div class="absolute bottom-0 right-0 text-gray-400 m-1 p-1 rounded bg-gray-800 bg-opacity-50">
+    <span class="indicator mr-2 text-white sm:text-gray-400">
       <phoneIcon />
       <div class="indicator-label">all</div>
     </span>
-    <span class="indicator mr-2 sm:text-gray-100 md:text-gray-300">
+    <span class="indicator mr-2 sm:text-white md:text-gray-400">
       <phoneLandscapeIcon />
       <div class="indicator-label">sm</div>
     </span>
-    <span class="indicator mr-2 md:text-gray-100 lg:text-gray-300">
+    <span class="indicator mr-2 md:text-white lg:text-gray-400">
       <tabletLandscapeIcon />
       <div class="indicator-label">md</div>
     </span>
-    <span class="indicator mr-2 lg:text-gray-100 xl:text-gray-300">
+    <span class="indicator mr-2 lg:text-white xl:text-gray-400">
       <laptopIcon />
       <div class="indicator-label">lg</div>
     </span>
-    <span class="indicator xl:text-gray-100">
+    <span class="indicator xl:text-white">
       <displayIcon />
       <div class="indicator-label">xl</div>
     </span>
