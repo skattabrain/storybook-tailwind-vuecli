314de0a285e412f52982e72d269dff497619d536
Greg Guepy <greg@guepy.com>
2020-09-05 21:01:07 -0400

    icon in app example

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>
-    This is an about page
-    <img
-      src="@/assets/icons/computer-desktop.svg"
-      class="inline-block w-3"
-    >
+  <h1 class="flex items-center">
+    This is an about page!
+    <CoolIcon class="ml-2 inline-block text-red-600 text-xl" />
+    <ScreenSizeIndicator />
   </h1>
 </template>
+
+
+<script>
+import CoolIcon from 'bootstrap-icons/icons/emoji-sunglasses.svg';
+import ScreenSizeIndicator from '@/components/utils/ScreenSizeIndicator';
+
+export default {
+  components: {
+    CoolIcon,
+    ScreenSizeIndicator
+  },
+};
+</script>
