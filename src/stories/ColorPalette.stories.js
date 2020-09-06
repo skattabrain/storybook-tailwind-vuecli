829342226ff5c00a1f8257d5e48b2382b93e4ebb
Greg Guepy <greg@guepy.com>
2020-09-05 22:15:43 -0400

    primping

--- src/stories/ColorPalette.stories.js
+++ src/stories/ColorPalette.stories.js
@@ -27,7 +27,7 @@ export const palette = () => ({
     <h3 class="capitalize font-semibold mb-2 text-lg text-gray-900">{{ color.name }}</h3>
     <div class="flex flex-wrap">
       <div v-for="variation in color.variations" class="w-1/2 md:w-1/3 flex flex-grow mb-3">
-        <div :class="variation.bg" class="border-solid border border-gray-300 rounded p-3 h-12 w-12 mr-4"></div>
+        <div :class="variation.bg" class="border-solid border border-gray-300 shadow-md rounded p-3 h-12 w-12 mr-4"></div>
         <div class="flex flex-col flex-grow items-start justify-center">
           <div class="capitalize font-semibold text-sm text-gray-700">{{ variation.color }}</div>
           <div class="font-mono text-xs text-pink-400">{{ variation.hex }}</div>
