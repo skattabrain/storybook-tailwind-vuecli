b458890c940d58ada9575271fd05079151b263ed
Greg Guepy <greg@guepy.com>
2020-09-07 10:05:44 -0400

    make more generic navigation

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,27 +1,18 @@
 <template>
-  <div class="bg-gray-800">
+  <div class="">
     <div class="container py-2">
-      <a
-        href="#"
-        class="inline-block rounded py-2 px-3 -ml-3 hover:bg-gray-900"
-      >
+      <a href="#"
+         class="inline-block py-2 px-3 -ml-3">
         <brand-logo />
       </a>
-      <ul
-        v-if="items"
-        class="inline-flex"
-      >
-        <li
-          v-for="item in items"
-          :key="item.label"
-          class="mr-2"
-        >
-          <a
-            class="inline-block rounded py-2 px-3 text-gray-300 hover:bg-gray-900 hover:text-gray-100"
-            :href="item.url"
-          >
-            {{ item.label }}
-          </a>
+      <ul v-if="items"
+          class="inline-flex">
+        <li v-for="item in items"
+            :key="item.label"
+            class="mr-2">
+          <a class="inline-block py-2 px-3"
+             :href="item.url">
+            {{ item.label }}</a>
         </li>
       </ul>
       <!-- <ul v-if="items" class="flex">
