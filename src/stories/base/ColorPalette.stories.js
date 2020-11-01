60325ada2988cc0a92e8b9ac639bbc1fd06772bd
Greg Guepy <greg@guepy.com>
2020-11-01 09:23:27 -0500

    primping 💅🏻

--- src/stories/base/ColorPalette.stories.js
+++ src/stories/base/ColorPalette.stories.js
@@ -9,16 +9,16 @@ export const palette = () => ({
     colors: colorMap(),
   }),
   template: `
-<div class="p-6">
+<div class="p-3">
   <div v-for="color in colors" class="mb-6">
     <h3 class="capitalize font-semibold mb-2 text-lg text-gray-900">{{ color.name }}</h3>
     <div class="flex flex-wrap">
-      <div v-for="variation in color.variations" class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 flex mb-6">
-        <div :class="variation.bg" class="border-solid border border-gray-300 shadow-md rounded p-3 h-20 w-20 mr-3"></div>
+      <div v-for="variation in color.variations" class="flex mb-6 w-1/2 sm:w-52">
+        <div :class="variation.bg" class="border-solid border border-gray-300 shadow-md rounded p-3 h-16 w-16 mr-4 sm:mr-2"></div>
         <div class="flex flex-col flex-grow items-start justify-center space-y-1">
           <div class="capitalize font-bold text-sm text-gray-700">{{ color.name }} {{ variation.color }}</div>
-          <div class="font-mono text-xs text-pink-600">.{{ variation.bg }}</div>
-          <div class="font-mono text-xs text-pink-400">{{ variation.hex }}</div>
+          <div class="font-mono text-xs text-red-500">x-{{ variation.suffix }}</div>
+          <div class="font-mono text-xs text-red-400 uppercase">{{ variation.hex }}</div>
         </div>
       </div>
     </div>
