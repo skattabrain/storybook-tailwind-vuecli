fd3bd494bde2df54c5b35c1222f00ae0d7415cbe
Greg Guepy <greg@guepy.com>
2020-11-19 08:14:56 -0500

    Move headings into typography

--- src/stories/base/Typography.stories.js
+++ src/stories/base/Typography.stories.js
@@ -1,15 +1,51 @@
 export default {
   title: 'Base/Typography',
+  argTypes: {
+    text: { control: 'text', name: 'Text' },
+  },
+  args: {
+    text: 'Taciti risus augue ligula orci tempor enim dui est',
+  },
 };
 
-export const Typography = () => ({
+export const Headings = (args) => ({
+  props: {
+    text: {
+      default: args.text
+    },
+  },
+
   template: `
 <div>
-  <div class="mb-2"><code>No Utility Class</code> - Habitant arcu mattis sociis pretium potenti ornare, leo diam tincidunt eu nisl, phasellus euismod vivamus cursus odio.</div>
-  <div class="mb-2 font-light"><code>.font-light</code> - Taciti risus augue ligula orci tempor enim dui est, rhoncus inceptos consectetur diam himenaeos curabitur dictum massa pharetra, aptent metus amet morbi non fames in.</div>
-  <div class="mb-2 font-normal"><code>.font-normal</code> - Class curabitur facilisi eget rhoncus donec montes mus tristique, volutpat cras leo habitasse ac lacus egestas, hendrerit ligula tincidunt nisi nam nostra eleifend.</div>
-  <div class="mb-2 font-bold"><code>.font-bold</code> - Integer arcu velit porttitor egestas laoreet commodo vulputate per, class vivamus dignissim nec erat torquent ultrices, lorem massa tortor proin est non amet.</div>
-  <div class="mb-2 font-extrabold"><code>.font-extrabold</code> - Habitant cras per blandit tristique aptent inceptos accumsan morbi eros vehicula dis turpis, et dictumst conubia ac sodales platea dictum habitasse nunc netus.</div>
+  <h1>H1 &mdash; {{ text }}</h1>
+  <h2>H2 &mdash; {{ text }}</h2>
+  <h3>H3 &mdash; {{ text }}</h3>
+  <h4>H4 &mdash; {{ text }}</h4>
+  <h5>H5 &mdash; {{ text }}</h5>
+  <h6>H6 &mdash; {{ text }}</h6>
+  <p>Paragraph &mdash; {{ text}} and this is <a href="#">a link</a>.</p>
+</div>
+`
+});
+
+export const Weights = (args) => ({
+  props: {
+    text: {
+      default: args.text
+    },
+  },
+  template: `
+<div>
+  <div class="mb-2"><code>No Utility Class</code> &mdash; {{ text }}</div>
+  <div class="mb-2 font-thin"><code>.font-thin</code> &mdash; {{ text }}</div>
+  <div class="mb-2 font-extralight"><code>.font-extralight</code> &mdash; {{ text }}</div>
+  <div class="mb-2 font-light"><code>.font-light</code> &mdash; {{ text }}</div>
+  <div class="mb-2 font-normal"><code>.font-normal</code> &mdash; {{ text }}</div>
+  <div class="mb-2 font-medium"><code>.font-medium</code> &mdash; {{ text }}</div>
+  <div class="mb-2 font-semibold"><code>.font-semibold</code> &mdash; {{ text }}</div>
+  <div class="mb-2 font-bold"><code>.font-bold</code> &mdash; {{ text }}</div>
+  <div class="mb-2 font-extrabold"><code>.font-extrabold</code> &mdash; {{ text }}</div>
+  <div class="mb-2 font-black"><code>.font-black</code> &mdash; {{ text }}</div>
 </div>
   `
 });
