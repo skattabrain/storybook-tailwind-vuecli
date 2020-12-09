052c0b627f458a458ee57af2e4a99c899a3cca58
Greg Guepy <greg@guepy.com>
2020-12-09 16:59:21 -0500

    add addon-a11y adon and update SB

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
   stories: ['../../src/stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+  ],
 }
