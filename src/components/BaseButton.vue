c939876e5d48f4993acba087efd45ae952a2f7e1
Greg Guepy <greg@guepy.com>
2020-12-09 20:00:17 -0500

    button starter

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="text-center" type="button">
+    <div class="absolute transition-opacity duration-100"
+         :class="working ? 'opacity-100' : 'opacity-0'">
+      <WorkingIcon class="animate-pulse" />
+    </div>
+    <div class="transition-opacity duration-100" :class="working ? 'opacity-0' : 'opacity-100'">
+      <slot />
+    </div>
+  </button>
+</template>
+
+<script>
+import WorkingIcon from 'bootstrap-icons/icons/three-dots.svg?inline';
+
+export default {
+  components: {
+    WorkingIcon
+  },
+  props: {
+    working: {
+      default: false,
+      type: Boolean
+    }
+  }
+}
+</script>
--- src/stories/base/Buttons.stories.js
+++ src/stories/base/Buttons.stories.js
@@ -1,23 +1,33 @@
+import BaseButton from '@/components/BaseButton.vue';
+
 export default {
   title: 'Base/Buttons',
   argTypes: {
     label: { control: 'text', name: 'Label' },
+    working: { control: 'boolean', name: 'Working' },
   },
   args: {
     label: 'Click Me',
+    working: false,
   },
 }
 
 export const Variants = (args) => ({
+  components: {
+    BaseButton,
+  },
   props: {
     label: {
       default: args.label
     },
+    working: {
+      default: args.working
+    }
   },
   template: `
 <div>
-  <button class="btn">{{ label }}</button>
-  <button class="btn btn--primary">{{ label }}</button>
+  <base-button class="btn" :working="working">{{ label }}</base-button>
+  <base-button class="btn btn--primary" :working="working">{{ label }}</base-button>
 </div>
 `
 })
