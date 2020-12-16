39896541cb0b6b7accceb95a8cd2d1cddf1bc508
Greg Guepy <greg@guepy.com>
2020-12-16 13:17:03 -0500

    add storybook-mobile for POC

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../../src/stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
+    // '@storybook/addon-a11y',
+    'storybook-mobile'
   ],
 }
